ca184fe0fa5c5c1db76d144777d7794983c90756
franco-cardoso <francolkk788@gmail.com>
2023-08-04 00:48:39 -0300

    Update scripts.ts

--- pages/api/scripts.ts
+++ pages/api/scripts.ts
@@ -1,6 +1,6 @@
 import * as jose from "jose";
 
-export const createToken = async (userID: string): Promise<string> => {
+export default const createToken = async (userID: string): Promise<string> => {
     return await new jose.SignJWT({ userID })
         .setProtectedHeader({ alg: "HS256" })
         .setIssuedAt()
