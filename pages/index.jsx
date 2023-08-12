0db450077e5f9e090976d4b6681404ed8e530599
franco-cardoso <francolkk788@gmail.com>
2023-08-12 17:26:26 -0300

    .

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import Board from "./components/home/Board";
 
 export const getStaticProps = async () => {
-    const req = await fetch(process.env.VERCEL_URL + "api/get-forums");
+    const req = await fetch(process.env.VERCEL_URL + "/api/get-forums");
     const data = await req.json();
     return { props: { data } };
 };
