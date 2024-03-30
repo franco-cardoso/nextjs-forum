22d794bfe1c0b97d7e64e794dbeb97cba436466c
franco-cardoso <francolkk788@gmail.com>
2024-03-30 15:33:50 -0300

    forum preloading

--- pages/forum/[forum].jsx
+++ pages/forum/[forum].jsx
@@ -1,20 +1,32 @@
+import Spinnger from "../components/misc/spinnger.svg";
 import { useRouter } from "next/router";
 import Sboard from "../components/home/board.module.css";
 import s from "./forum.module.css";
 import Thread from "./Thread";
 import Link from "next/link";
 import { getCookie } from "cookies-next";
+import { useEffect, useState } from "react";
+import Image from "next/image";
 
 export const getServerSideProps = async ({ params }) => {
-    const data = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/get-threads?forum=${params.forum}`);
-    const threads = await data.json();
+    // const data = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/get-threads?forum=${params.forum}`);
+    // const threads = await data.json();
 
-    return { props: { threads } };
+    return { props: { forum: params.forum } };
 };
 
-export default function Forum({ threads }) {
-    const forum = useRouter().query.forum;
-    
+export default function Forum({ forum }) {
+    // const forum = useRouter().query.forum;
+    const [threads, setThreads] = useState(null);
+
+    useEffect(() => {
+        fetch(`${process.env.NEXT_PUBLIC_URL}/api/get-threads?forum=${forum}`)
+            .then((res) => {
+                res.json().then((res) => setThreads(res));
+            })
+            .catch((err) => console.log(err));
+    }, [forum]);
+
     return (
         <section className={Sboard["section"]}>
             <Link href={getCookie("jwt") ? `/forum/create-thread?forum=${forum}` : "/login"}>Create Thread</Link>
@@ -24,10 +36,23 @@ export default function Forum({ threads }) {
             <table className={Sboard["table"] + " " + s["table"]}>
                 <tbody>
                     {threads?.map((el) => (
-                        <Thread title={el.title} date={el.date} author={el.author} key={el.id} id={el.id} forum={forum} views={el.views}></Thread>
+                        <Thread
+                            title={el.title}
+                            date={el.date}
+                            author={el.author}
+                            key={el.id}
+                            id={el.id}
+                            forum={forum}
+                            views={el.views}
+                        ></Thread>
                     ))}
                 </tbody>
             </table>
+            {!threads && (
+                <div style={{ padding: "40px 0 40px 0" }}>
+                    <Image src={Spinnger} height={80}></Image>
+                </div>
+            )}
         </section>
     );
 }
