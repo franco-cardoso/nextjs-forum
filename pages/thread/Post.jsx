35c7ab08e1ea64212c591d7345df3f6e89ff0776
franco-cardoso <francolkk788@gmail.com>
2024-07-12 05:51:27 -0300

    .

--- pages/thread/Post.jsx
+++ pages/thread/Post.jsx
@@ -1,25 +1,25 @@
-import s from "./thread.module.css";
-import User from "../components/misc/User";
-import { DateTime } from "luxon";
+// import s from "./thread.module.css";
+// import User from "../components/misc/User";
+// import { DateTime } from "luxon";
 
-function Post(data) {
-    const { author, content, date } = data.data;
-    // const dateFormat = DateTime.fromSeconds(+date).toRelative()
+// function Post(data) {
+//     const { author, content, date } = data.data;
+//     const dateFormat = DateTime.fromSeconds(+date).toRelative()
 
-    return (
-        <div className={s["post"]}>
-            <div className={s["post-profile"]}>
-                <User username={author}></User>
-                <img src="https://picsum.photos/140/140" alt="" />
-            </div>
-            <div className={s["post-content-wrapper"]}>
-                <div>
-                    <span className={s["post-date"]}>{date}</span>
-                </div>
-                <div className={s["post-content"]}>{content}</div>
-            </div>
-        </div>
-    );
-}
+//     return (
+//         <div className={s["post"]}>
+//             <div className={s["post-profile"]}>
+//                 <User username={author}></User>
+//                 <img src="https://picsum.photos/140/140" alt="" />
+//             </div>
+//             <div className={s["post-content-wrapper"]}>
+//                 <div>
+//                     <span className={s["post-date"]}>{dateFormat}</span>
+//                 </div>
+//                 <div className={s["post-content"]}>{content}</div>
+//             </div>
+//         </div>
+//     );
+// }
 
-export default Post;
+// export default Post;
--- pages/thread/[thread].jsx
+++ pages/thread/[thread].jsx
@@ -1,40 +1,49 @@
-import { useRouter } from "next/router";
-import React, { useContext } from "react";
-import { GlobalContext } from "../_app";
-import s from "./thread.module.css";
-import { useEffect } from "react";
-import Post from "./Post";
-import AddPost from "./AddPost";
+// import { useRouter } from "next/router";
+// import React, { useContext } from "react";
+// import { GlobalContext } from "../_app";
+// import s from "./thread.module.css";
+// import { useEffect } from "react";
+// import Post from "./Post";
+// import AddPost from "./AddPost";
 
-export const getServerSideProps = async ({ params }) => {
-    const req1 = await fetch(process.env.NEXT_PUBLIC_URL + `/api/get-thread?thread=${params.thread}`);
-    const threadData = await req1.json();
-
-    const req2 = await fetch(process.env.NEXT_PUBLIC_URL + `/api/get-posts?thread=${params.thread}`)
-    const posts = await req2.json()
-
-    return { props:  {threadData: threadData[0], posts: posts.length ? posts : null}  };
-};
-
-function Thread({threadData, posts}) {
-    const { currentUser } = useContext(GlobalContext);
-
-    // useEffect(() => {
-    //     if (thread) fetch(`/api/add-view?t=${thread}`);
-    // }, [thread]);
-
-    return (
-        <div className={s["thread-wrapper"]}>
-            <Post data={threadData}></Post>
-            {
-                posts?.map((el) => (
-                    <Post data={el}></Post>
-
-                ))
-            }
-            <AddPost threadId={threadData.id}></AddPost>
-        </div>
-    );
+function Thread() {
+    return ( 
+        <div></div>
+     );
 }
 
 export default Thread;
+
+
+// export const getServerSideProps = async ({ params }) => {
+//     const req1 = await fetch(process.env.NEXT_PUBLIC_URL + `/api/get-thread?thread=${params.thread}`);
+//     const threadData = await req1.json();
+
+//     const req2 = await fetch(process.env.NEXT_PUBLIC_URL + `/api/get-posts?thread=${params.thread}`)
+//     const posts = await req2.json()
+
+//     return { props:  {threadData: threadData[0], posts: posts.length ? posts : null}  };
+// };
+
+// function Thread({threadData, posts}) {
+//     const { currentUser } = useContext(GlobalContext);
+
+//     // useEffect(() => {
+//     //     if (thread) fetch(`/api/add-view?t=${thread}`);
+//     // }, [thread]);
+
+//     return (
+//         <div className={s["thread-wrapper"]}>
+//             <Post data={threadData}></Post>
+//             {
+//                 posts?.map((el) => (
+//                     <Post data={el}></Post>
+
+//                 ))
+//             }
+//             <AddPost threadId={threadData.id}></AddPost>
+//         </div>
+//     );
+// }
+
+// export default Thread;
